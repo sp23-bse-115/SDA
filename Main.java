98a00d8127f80b0c95dd37bc9aa1cbd17a15ae8b
FA21-BSE-134 <FA21-BSE-134@network.dcui>
2025-03-12 12:12:00 +0500

    with principle

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        UserController userController = new UserController();
+        userController.addUser ("Alice");
+        userController.addUser ("Bob");
+        userController.listUsers();
+    }
+}
