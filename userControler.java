a3d8aa1ef0945e2c41a2171c7a56764a93333775
sp23-bse-115 <145669575+sp23-bse-115@users.noreply.github.com>
2025-03-18 22:40:12 +0500

    Create userControler.java

---
+++ userControler.java
@@ -0,0 +1,12 @@
+class UserController {
+    private List<User> users;
+
+    public UserController() {
+        users = new ArrayList<>();
+    }
+
+    public void addUser (String name) {
+        User user = new User(name);
+        users.add(user);
+        System.out.println("User  added: " + name);
+    }
